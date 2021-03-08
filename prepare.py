dd4e75b2c86ade48aad6ce6fd58db4ee753bc0bc
John Grinstead <john.allen.grinstead@gmail.com>
2021-03-08 09:43:55 -0600

    completed prepare

--- prepare.py
+++ prepare.py
@@ -32,11 +32,11 @@ def split(df, stratify_by='churn'):
     """
     
     if stratify_by == None:
-        train, test = train_test_split(df, test_size=.2, random_state=123)
-        train, validate = train_test_split(train, test_size=.3, random_state=123)
+        train, test = train_test_split(df, test_size=.2, random_state=319)
+        train, validate = train_test_split(train, test_size=.3, random_state=319)
     else:
-        train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
-        train, validate = train_test_split(train, test_size=.3, random_state=123, stratify=train[stratify_by])
+        train, test = train_test_split(df, test_size=.2, random_state=319, stratify=df[stratify_by])
+        train, validate = train_test_split(train, test_size=.3, random_state=319, stratify=train[stratify_by])
     
     return train, validate, test
 
